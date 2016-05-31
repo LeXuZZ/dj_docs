0264b732695c1de0c3876d1dfcfc8114a9f5eb30
dsmyk <porovozls@gmail.com>
2016-05-31 12:56:38 +0300

    add poll views

--- poll/urls.py
+++ poll/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from poll.views import *
 
 urlpatterns = [
+    url(r'^api/v1/poll/$', PollView.as_view()),
+    url(r'^api/v1/poll/(?P<id>\d+)/$', PollView.as_view()),
     url(r'^api/v1/get_csrf_token/$', get_csrf),
     url(r'^api/v1/register/$', RegisterView.as_view()),
     url(r'^api/v1/registration_confirmation/(?P<registration_hash>\w+)', RegisterView.as_view()),
--- poll/views.py
+++ poll/views.py
@@ -10,22 +10,42 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.views.generic import View
-from poll.models import PollUser
+from poll.models import *
 from dj_docs.settings import redis_instance, REGISTRATION_EXPIRATION_TIME
 
 
 def response_message(success=False, data=None, **kwargs):
     allowed_keys = ('text', 'info', 'warning', 'error')
-    return json.dumps({'success': success,
-                       'data': data,
-                       'message': {
-                           k: v for k, v in kwargs.items() if k in allowed_keys
-                           }})
+    return HttpResponse(json.dumps({'success': success,
+                                    'data': data,
+                                    'message': {
+                                        k: v for k, v in kwargs.items() if k in allowed_keys
+                                        }}))
 
 
 @ensure_csrf_cookie
 def get_csrf(request):
     return HttpResponse("")
+
+
+class PollView(View):
+    def get(self, request, id=None):
+        if id:
+            try:
+                poll = Poll.objects.get(pk=id)
+                return response_message(success=True, data=poll.json_serialize())
+            except Poll.DoesNotExist:
+                return response_message(success=False, error=True, text='no such poll')
+
+        return response_message(success=True, data=[poll.json_serialize() for poll in Poll.objects.all()])
+
+    def post(self, request):
+        name, json = request.POST.get("name"), request.POST.get("json")
+        try:
+            Poll.objects.create(name=name, json=json).save()
+            return response_message(success=True)
+        except Exception:
+            return response_message(success=False, error=True, text='cannot save poll to db')
 
 
 class IndexView(View):
@@ -41,10 +61,10 @@ class LoginView(View):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return HttpResponse(response_message(success=True))
-        return HttpResponse(response_message(success=False,
-                                             text='Невірно вказана пара логін\пароль',
-                                             error=True))
+                return response_message(success=True)
+        return response_message(success=False,
+                                text='Невірно вказана пара логін\пароль',
+                                error=True)
 
 
 class RegisterView(View):
@@ -61,33 +81,32 @@ class RegisterView(View):
         })
         redis_instance.setex(registration_hash, REGISTRATION_EXPIRATION_TIME, registration_data)
         redis_instance.publish('registration_email_channel', registration_data)
-        return HttpResponse(response_message(success=True,
-                                             text='email with registration link was send'))
+        return response_message(success=True,
+                                text='email with registration link was send')
 
     def get(self, request, registration_hash):
         try:
             registration_data = json.loads(redis_instance.get(registration_hash).decode())
         except AttributeError:
-            return HttpResponse(response_message(success=False,
-                                                 text='such registration key was not found',
-                                                 error=True))
+            return response_message(success=False,
+                                    text='such registration key was not found',
+                                    error=True)
 
         user = PollUser(email=registration_data.get('email'))
         user.set_password(registration_data.get('password'))
         try:
             user.is_active = True
             user.save()
-            return HttpResponse(response_message(success=True,
-                                                 text='u have registered',
-                                                 info=True))
+            return response_message(success=True,
+                                    text='u have registered',
+                                    info=True)
         except IntegrityError:
-            return HttpResponse(response_message(success=False,
-                                                 error=True,
-                                                 text='such user already exists'))
-
+            return response_message(success=False,
+                                    error=True,
+                                    text='such user already exists')
 
 
 class LogoutView(View):
     def post(self, request):
         logout(request)
-        return HttpResponse(response_message(success=True))
+        return response_message(success=True)
